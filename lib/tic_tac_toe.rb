c85856628b57e73e70cf77527da25fab6394cf45
matthew.serre <matthew.serre@gmail.com>
2018-10-09 00:33:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,7 @@ class TicTacToe
   end
 
   def input_to_index(userInput)
-    @index = userInput.to_i - 1
+    @index = userInput.to_int - 1
   end
 
   def move(boardArray, indexToFill, xOrO = "X")

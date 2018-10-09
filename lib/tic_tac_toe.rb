35b6fa7a40470d497e321a9ca14c91b4e07ed641
matthew.serre <matthew.serre@gmail.com>
2018-10-09 00:47:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    if position_taken?(board,index) == false && index.between?(0,8) == true
+    if position_taken?(@board,index) == false && index.between?(0,8) == true
       true
     else
       false

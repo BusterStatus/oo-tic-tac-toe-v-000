f23e347eee395fbd325178657b9a4cb9525f5965
matthew.serre <matthew.serre@gmail.com>
2018-10-09 00:23:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,14 +16,16 @@ class TicTacToe
   
   def display_board=(board)
     @board = board
-  end
-  
-  def display_board
+    
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def display_board
+
   end
 
   def input_to_index(userInput)
